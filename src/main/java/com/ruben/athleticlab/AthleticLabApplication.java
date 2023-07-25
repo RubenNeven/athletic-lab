b778421073054ea10746be00030c70afb3dcb719
RubenNeven <rubenneven@gmail.com>
2023-07-25 11:39:21 +0200

    Athletic-Lab Init Project

---
+++ src/main/java/com/ruben/athleticlab/AthleticLabApplication.java
@@ -0,0 +1,13 @@
+package com.ruben.athleticlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleticLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthleticLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruben/athleticlab/AthleticLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruben.athleticlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthleticLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
